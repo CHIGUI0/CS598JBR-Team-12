549b0ddf472c5a5dbc80a76d26f2f94dc0ba834a
GUI, Chi <107296354+CHIGUI0@users.noreply.github.com>
2024-09-21 19:36:05 -0500

    Update model_prompting.py

--- MP1/model_prompting.py
+++ MP1/model_prompting.py
@@ -16,19 +16,40 @@ def prompt_model(dataset, model_name = "deepseek-ai/deepseek-coder-6.7b-base", q
     print(f"Working with {model_name} quantization {quantization}...")
     
     # TODO: download the model
-    
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
   
     if quantization:
         # TODO: load the model with quantization
+        model = AutoModelForCausalLM.from_pretrained(
+        pretrained_model_name_or_path=model_name,
+        load_in_4bit=True,
+        device_map='auto',
+        # max_memory=max_memory,
+        torch_dtype=torch.bfloat16,
+        quantization_config=BitsAndBytesConfig(
+            load_in_4bit=True,
+            bnb_4bit_compute_dtype=torch.bfloat16,
+            bnb_4bit_use_double_quant=True,
+            bnb_4bit_quant_type='nf4'
+        ),)
         
     else:
         # TODO: load the model without quantization
-        
+        pass
 
     results = []
     for case in dataset:
         prompt = case['prompt']
         # TODO: prompt the model and get the response
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        
+        input_ids = tokenizer(prompt, return_tensors="pt").input_ids.to(device)
+
+        # Generate output from the model
+        output = model.generate(input_ids, max_length=500, num_return_sequences=1)
+        
+        # Decode and print the output
+        response = tokenizer.decode(output[0], skip_special_tokens=True, temperature=0)
         
         print(f"Task_ID {case['task_id']}:\nPrompt:\n{prompt}\nResponse:\n{response}")
         results.append(dict(task_id=case["task_id"], completion=response))
